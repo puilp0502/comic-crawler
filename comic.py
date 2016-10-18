62dfbf8b87b6f07f128c4a9c6dd8de2e5ff62778
Frank Yang <puilp0502@gmail.com>
2016-10-19 07:16:14 +0900

    WIP

---
+++ comic.py
@@ -0,0 +1,6 @@
+class Comic(object):
+    def __init__(self, name, site, thumbnail_url, url):
+        self.name = name
+        self.site = site
+        self.thumbnail_url = thumbnail_url
+        self.url = url
